0e1668cc960ccd15a5fb32f5de0536f98cace61b
subho004 <91646273+subho004@users.noreply.github.com>
2023-03-04 09:33:01 +0530

    final changes in villaAPIController.cs to make the patch work correctly

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -4,6 +4,7 @@ using MagicVilla_VillaAPI.Models;
 using MagicVilla_VillaAPI.Models.DTO;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Numerics;
 
 namespace MagicVilla_VillaAPI.Controllers
@@ -158,7 +159,7 @@ namespace MagicVilla_VillaAPI.Controllers
                 return BadRequest();
             }
             //var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
-            var villa = _db.Villas.FirstOrDefault(u => u.Id == id);
+            var villa = _db.Villas.AsNoTracking().FirstOrDefault(u => u.Id == id);
 
             VillaDTO villaDTO = new()
             {
